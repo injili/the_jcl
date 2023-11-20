30e9d9255af8e92e438578bc553051a291f4dbec
injili <nyarekigospel@gmail.com>
2023-11-20 19:35:37 +0300

    new ting

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, redirect, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """
+    The index file url
+    """
+    return (render_template('index.html'))
+
+@app.route('/product', strict_slashes=False)
+def product():
+    """
+    The function to the product page
+    """
+    return (render_template('product.html'))
+
+@app.route('/contacts', strict_slashes=False)
+def contact():
+    """
+    The route to the contacts page
+    """
+    return (render_template('contact.html'))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=888)
